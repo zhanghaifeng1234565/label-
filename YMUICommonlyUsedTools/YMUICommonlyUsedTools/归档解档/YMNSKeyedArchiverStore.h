68b13bb2a18b802940ed184b1723f7f2e8708ad3
iOS <ios@iosdevdeMacBook-Air.local>
2018-05-24 11:13:54 +0800

    注释
    增加必要的解释说明

--- YMUICommonlyUsedTools/YMUICommonlyUsedTools/归档解档/YMNSKeyedArchiverStore.h
+++ YMUICommonlyUsedTools/YMUICommonlyUsedTools/归档解档/YMNSKeyedArchiverStore.h
@@ -5,6 +5,13 @@
 //  Created by iOS on 2018/5/23.
 //  Copyright © 2018年 iOS. All rights reserved.
 //
+/**
+ Documents[NSDocumentDirectory]【iTunes 会备份该目录。一般用来存储需要持久化的数据】
+ Library
+    Caches[NSCachesDirectory]【缓存，iTunes 不会自动备份该目录。内存不足时会被清除，应用没有运行时可能会被清除。一般存储体积大、不需要备份的非重要信息】
+    Preferences【iTunes 会备份该目录，可以来存储一些偏好设置】【可以使用 YMPreferencesStore】
+ tmp[NSString *tmp= NSTemporaryDirectory()]【iTunes 不会备份这个目录，用来存储临时数据，应用退出时会清除该目录下载的数据】
+ */
 
 #import <Foundation/Foundation.h>
 
